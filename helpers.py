e95dcb4ff20e42d8d0ba670ff7751c7aa08e46e4
lniederhauser <lucille.niederhauser@epfl.ch>
2023-05-05 22:10:20 +0200

    Start of question 1.3

--- helpers.py
+++ helpers.py
@@ -50,7 +50,7 @@ def simulate_soma(tau_s, C, v_rest, b, v_spike, tau_w, I_stim, T_refractory, sim
     neuron.w = 0.0 * b2.pA
 
     # Monitoring membrane voltage (v) and w
-    #state_monitor = b2.StateMonitor(neuron, ["v", "w", "I_stim"], record=True)
+    # state_monitor = b2.StateMonitor(neuron, ["v", "w", "I_stim"], record=True)
     # TODO: Find out if we have to record I_stim and how ??
     #  all state monitor can record are {'v', 'lastspike', 'not_refractory', 'w'}
     state_monitor = b2.StateMonitor(neuron, True, record=True)
@@ -59,8 +59,6 @@ def simulate_soma(tau_s, C, v_rest, b, v_spike, tau_w, I_stim, T_refractory, sim
     # running simulation
     b2.run(simulation_time)
     return state_monitor, spike_monitor
-
-
 
 
 ########################## DENDRITIC COMPARTMENT ################################
@@ -113,6 +111,24 @@ def simulate_dendritic(tau_d, C, v_rest, a, tau_w, E_d, D_d, g, I_stim, simulati
     b2.run(simulation_time)
 
     return state_monitor
+
+
+def extract_rising_voltage_trace(state_monitor):
+    voltage_array = b2.asarray(state_monitor.v)
+    voltage_array = np.squeeze(voltage_array)
+    id_max = voltage_array.argmax(axis=0)
+    rising_voltage = voltage_array[:id_max]
+
+    return rising_voltage
+
+
+def compute_finite_derivative(voltage_trace):
+    finite_derivative = np.zeros(voltage_trace.shape)
+    finite_derivative[0] = voltage_trace[0]
+    for i in range(1, voltage_trace.size):
+        finite_derivative[i] = voltage_trace[i] - voltage_trace[i-1]
+
+    return finite_derivative
 
 
 ######################################### PLOTTING ######################################################
@@ -171,30 +187,24 @@ def plot_I_v_w(voltage_monitor, current, title=None, firing_threshold=None, lege
     plt.show()
 
 
-def extract_rising_voltage_trace(state_monitor):
-    voltage_array = b2.asarray(state_monitor.v)
-    voltage_array = np.squeeze(voltage_array)
-    id_max = voltage_array.argmax(axis=0)
-    rising_voltage = voltage_array[:id_max]
-
-    return rising_voltage
-
-
-def compute_finite_derivative(voltage_trace):
-    finite_derivative = np.zeros(voltage_trace.shape)
-    finite_derivative[0] = voltage_trace[0] # TODO: find out how to compute first data point
-    for i in range(1, voltage_trace.size):
-        finite_derivative[i] = voltage_trace[i] - voltage_trace[i-1]
-
-    return finite_derivative
-
-
 def plot_voltage_derivative_curve(voltage, derivative, title=None, save_figure=False):
     plt.plot(voltage * 1000, derivative * 1000)
-    plt.xlabel("voltage [mA]")
-    plt.ylabel("finite difference derivative [mA/ms]")
+    plt.xlabel("voltage [mV]")
+    plt.ylabel("finite difference derivative [mV/ms]")
     plt.grid()
     plt.title(title)
     if save_figure:
         plt.savefig("plots/" + title + ".png")
+    plt.show()
+
+
+def plot_sigmoid(x, E, D, save_figure=False):
+    E = float(E)
+    D = float(D)
+    y = 1/(1+np.exp(-(x-E)/D))
+    plt.plot(x*1000, y)
+    plt.grid()
+    plt.xlabel("voltage [mV]")
+    if save_figure:
+        plt.savefig("plots/sigmoid.png")
     plt.show()
