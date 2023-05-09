55ccfd941b0ce20807df36a55b07a71dcb279bf0
Caleiga <julian.bar@epfl.ch>
2023-05-09 18:13:13 +0200

    part 2 function added but both systems are still seperate

--- helpers.py
+++ helpers.py
@@ -130,6 +130,62 @@ def compute_finite_derivative(voltage_trace):
 
     return finite_derivative
 
+######################################### TWO COMPARTMENT ######################################################
+def simulate_pyramidal_neuron(tau_s, tau_d, C_s, C_d, v_rest, b, v_spike, tau_w_s, tau_w_d, I,
+                              a, E_d, D_d, g, T_refractory, simulation_time=200 * b2.ms):
+    r"""
+    Simulation of the soma compartment using model equations and refractory period
+
+    The Brian2 model equations are:
+
+    .. math::
+
+        \frac{dv}{dt} = -\frac{v-v_rest}{tau_s} + \frac{I_stim+w}{C}
+        \\
+        \frac{dw}{dt} = \frac{-w}{tau_w}
+
+    Args:
+        tau_s (Quantity): membrane time scale
+        C (Quantity): membrane capacitance
+        v_rest (Quantity): resting potential
+        b (Quantity): Spike-triggered adaptation current (=increment of w after each spike)
+        v_spike (Quantity): voltage threshold for the spike condition
+        tau_w (Quantity): Adaptation time scale
+        I_s (TimedArray): Input current at Soma
+        T_refractory (Quantity): Refractory period
+        simulation_time (Quantity): Duration for which the model is simulated
+
+    Returns:
+        (state_monitor, spike_monitor):
+        A b2.StateMonitor for the variables "v" and "w" and a b2.SpikeMonitor
+    """
+
+    # EXP-IF
+    # "unless refractory" arg allows voltage to stay unchanged during the refractory period
+    eqs = """
+        dv_s/dt = -(v_s - v_rest)/tau_s + (I(t,i) + w_s)/C_s : volt (unless refractory)
+        dw_s/dt =-w_s/tau_w_s : amp
+        dv_d/dt = -(v_d-v_rest)/tau_d + (I(t,i) + w_d + g*(1 /(1+exp(-(v_d-E_d)/D_d))))/C_d : volt
+        dw_d/dt=(-w_d + a * (v_d - v_rest))/tau_w_d : amp
+        """
+
+    neuron = b2.NeuronGroup(1, model=eqs, threshold="v_s>v_spike", reset="v_s=v_rest;w_s+=b;v_d=v_d;w_d=w_d", refractory=T_refractory,
+                            method="euler")
+    
+    # print(neuron)
+
+    # initial values of v and w is set here:
+    neuron.v_s = v_rest
+    neuron.w_s = 0.0 * b2.pA
+    neuron.v_d = v_rest
+    neuron.w_d = 0.0 * b2.pA
+
+    state_monitor = b2.StateMonitor(neuron, ["v_s", "v_d", "w_s", "w_d"], record=True)
+    #spike_monitor = b2.SpikeMonitor(neuron)
+
+    b2.run(simulation_time)
+    return state_monitor#, spike_monitor
+
 
 ######################################### PLOTTING ######################################################
 def plot_I_v_w(voltage_monitor, current, title=None, firing_threshold=None, legend_location=0, savefig = False):
@@ -188,11 +244,12 @@ def plot_I_v_w(voltage_monitor, current, title=None, firing_threshold=None, lege
 
 
 def plot_voltage_derivative_curve(voltage, derivative, title=None, save_figure=False):
+    
     plt.plot(voltage * 1000, derivative * 1000)
-    plt.xlabel("voltage [mV]")
-    plt.ylabel("finite difference derivative [mV/ms]")
+    plt.xlabel("Voltage [mV]", fontsize=12)
+    plt.ylabel("Finite difference derivative [mV/ms]", fontsize=12)
     plt.grid()
-    plt.title(title)
+    plt.title(title, fontsize=14)
     if save_figure:
         plt.savefig("plots/" + title + ".png")
     plt.show()
@@ -207,4 +264,70 @@ def plot_sigmoid(x, E, D, save_figure=False):
     plt.xlabel("voltage [mV]")
     if save_figure:
         plt.savefig("plots/sigmoid.png")
+    plt.show()
+
+
+
+def plot_pyramidal(voltage_monitor, current, title=None, firing_threshold=None, legend_location=0, savefig = False):
+    """plots voltage and current .
+
+    Args:
+        voltage_monitor (StateMonitor): recorded voltage
+        current (TimedArray): injected current
+        title (string, optional): title of the figure
+        firing_threshold (Quantity, optional): if set to a value, the firing threshold is plotted.
+        legend_location (int): legend location. default = 0 (="best")
+
+    Returns:
+        the figure
+    """
+
+    assert isinstance(voltage_monitor, b2.StateMonitor), "voltage_monitor is not of type StateMonitor"
+    assert isinstance(current, b2.TimedArray), "current is not of type TimedArray"
+
+    time_values_ms = voltage_monitor.t / b2.ms
+
+    fig, ax = plt.subplots(5, 1, figsize=(10, 15))
+
+    # Plot the input current I
+    c = current(voltage_monitor.t, 0)
+    ax[0].plot(voltage_monitor.t / b2.ms, c /b2.nA, "r", lw=2)
+    ax[0].set_ylabel("Input current [nA]", fontsize=12)
+    ax[0].grid()
+
+    # Plot the Soma voltage v_s
+    ax[1].plot(time_values_ms, voltage_monitor[0].v_s / b2.mV, lw=2)
+    if firing_threshold is not None:
+        ax[1].plot(
+            (voltage_monitor.t / b2.ms)[[0, -1]],
+            [firing_threshold / b2.mV, firing_threshold / b2.mV],
+            "r--", lw=2)
+        ax[1].legend(["vm", "firing threshold"], fontsize=12, loc=legend_location)
+        
+    ax[1].set_ylabel("Soma Membrane Voltage [mV]", fontsize=12)
+    ax[1].grid()
+
+    # Plot the Soma adaptive term w_s
+    ax[2].plot(time_values_ms, voltage_monitor[0].w_s /b2.nA, lw=2)
+    ax[2].set_ylabel("Soma Adaption Variable [nA]", fontsize=12)
+    ax[2].grid()
+
+    # Plot the dendrite voltage v_d
+    ax[3].plot(time_values_ms, voltage_monitor[0].v_d /b2.nA, lw=2)
+    ax[3].set_ylabel("Dendrite Membrane Voltage [mV]", fontsize=12)
+    ax[3].grid()
+
+    # Plot the dendrite adaptive term w_d
+    ax[4].plot(time_values_ms, voltage_monitor[0].w_d /b2.nA, lw=2)
+    ax[4].set_xlabel("t [ms]", fontsize=12)
+    ax[4].set_ylabel("Dendrite Adaption Variable [nA]", fontsize=12)
+    ax[4].grid()
+
+    if title is not None:
+        fig.suptitle(title, fontsize=14)
+
+    fig.tight_layout()
+
+    if savefig:
+        plt.savefig("plots/" + title + ".png")
     plt.show()
